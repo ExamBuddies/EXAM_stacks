705258642582b2f603b62258c17bae552c4b655f
washer <madspelt@gmail.com>
2015-05-20 20:06:24 +0200

    linked list done (very basic)

--- src/queues/QueueDemo.java
+++ src/queues/QueueDemo.java
@@ -1,5 +1,15 @@
 package queues;
 
 public class QueueDemo {
-
+	public static void main(String[] args){
+		QueueLinked<Integer> queueLinked = new QueueLinked<Integer>();
+		
+		queueLinked.enqueue(1);
+		queueLinked.enqueue(2);
+		queueLinked.enqueue(3);
+		
+		System.out.println(queueLinked.dequeue());
+		System.out.println(queueLinked.dequeue());
+		System.out.println(queueLinked.dequeue());
+	}
 }
--- src/queues/QueueLinked.java
+++ src/queues/QueueLinked.java
@@ -1,12 +1,37 @@
 package queues;
 
-public class QueueLinked {
+public class QueueLinked <E> implements Queue <E>{
 	private QLnode front, back;
-	private int size;
 	
 	public QueueLinked(){
 		front = null;
 		back = null;
-		size = 0;
 	}
+
+	@Override
+	public E dequeue() {
+		if(front == null){
+			return null;
+		}
+		else{
+			E data = (E)front.data;
+			front = front.previous;
+			return data;
+		}
+	}
+
+	@Override
+	public void enqueue(E data) {
+		if(front == null && back == null){
+			QLnode newNode = new QLnode(data);
+			front = newNode;
+			back  = newNode;
+		}
+		else{
+			QLnode newNode = new QLnode(data);
+			back.previous = newNode;
+			back = newNode;
+		}
+	}
+
 }
